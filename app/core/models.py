536eb4fee5edfbbca97f593dec3190a3d7922e31
Nikita <nikitabechthold@gmail.com>
2023-09-16 11:27:43 +0600

    Fixed bug with email normalization

--- app/core/models.py
+++ app/core/models.py
@@ -14,7 +14,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **kwargs):
         """Create and return a new user."""
-        user = self.model(email=email, **kwargs)
+        user = self.model(email=self.normalize_email(email), **kwargs)
         user.set_password(password)
         user.save()
 
